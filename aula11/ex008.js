e719e6a5f6dfd9ac4a625bf3f3833a2cc3f3e85b
DaniloCesar021 <129341712+DaniloCesar021@users.noreply.github.com>
2024-03-18 02:33:42 -0300

    if
    else

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Vc ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija usando sempre cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Eua' 
+if (pais == 'brasil') {
+    console.log('Brasileiro')
+}else {
+    console.log('gringo')
+}   
