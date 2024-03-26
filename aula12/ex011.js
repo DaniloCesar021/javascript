3c1db648cc75043f005b2395744ea645ec5edee9
DaniloCesar021 <129341712+DaniloCesar021@users.noreply.github.com>
2024-03-26 20:57:02 -0300

    comitei
    comitei

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,9 +1,9 @@
-var idade = 68
+var idade = 16
 console.log(`vc tem ${idade} anos.`)
 if (idade < 16) {
     console.log('Não vota') 
    
-}  else if ( idade < 18 ,  idade > 67) {
+}  else if ( idade < 18 || idade > 67) {
         console.log('voto opicinal  ')
 } else {
     console.log ('voto obrigatorio')
